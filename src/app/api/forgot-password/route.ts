fe26ece2213d52087deed743a3ad197bbef3d18b
samia-urooj <wajeehaurooj13@gmail.com>
2025-03-08 21:38:44 +0500

    commit changes

--- src/app/api/forgot-password/route.ts
+++ src/app/api/forgot-password/route.ts
@@ -5,7 +5,9 @@ import nodemailer from 'nodemailer';
 
 // Nodemailer Setup
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER, // Your email
     pass: process.env.EMAIL_PASS, // Your email password
